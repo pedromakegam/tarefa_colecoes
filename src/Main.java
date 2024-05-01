72bceff7044d120ba539b96f24b73e900160f766
pedrovanderlansantana <pedromakegam@gmail.com>
2024-05-01 19:22:46 -0300

    e

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        AlphabeticSort();
+        ClassificacaoPorGenero();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void AlphabeticSort(){
+        List<String> names = new ArrayList<>();
+        Scanner nomes_input = new Scanner(System.in);
+        for(int i = 0; i<3; i++){
+            System.out.println("Digite o nome: ");
+            names.add(nomes_input.nextLine());
         }
+        Collections.sort(names);
+        System.out.println("NOMES ORDENADOS EM ORDEM ALFABETICA" + names);
+    }
+    public static void ClassificacaoPorGenero(){
+        List<Pessoas> pessoas = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Scanner nome_input = new Scanner(System.in);
+            System.out.println("Digite o nome: ");
+            String nome = nome_input.nextLine();
+            Scanner genero_input = new Scanner(System.in);
+            System.out.println("Digite o genero: ");
+            String genero = genero_input.nextLine();
+            pessoas.add(new Pessoas(nome, genero));
+        }
+//        for (Pessoas pessoa : pessoas) {
+//            if (pessoa.getGenero().equals("M")||pessoa.getGenero().equals("Masculino")) {
+//                System.out.println("Pessoa do gênero masculino: " + pessoa.getNome());
+//            } else if (pessoa.getGenero().equals("F") || pessoa.getGenero().equals("Feminino")) {
+//                System.out.println("Pessoa do gênero feminino: " + pessoa.getNome());
+//            }
+//        }
+        List<String> pessoas_do_sexo_masculino = new ArrayList<>();
+        List<String> pessoas_do_sexo_feminino = new ArrayList<>();
+        for (Pessoas pessoa:pessoas){
+            if (pessoa.getGenero().equals("M")||pessoa.getGenero().equals("Masculino")){
+                pessoas_do_sexo_masculino.add(pessoa.getNome());
+            }else if (pessoa.getGenero().equals("F")||pessoa.getGenero().equals("Feminino")){
+                pessoas_do_sexo_feminino.add(pessoa.getNome());
+            }
+        }
+
+        System.out.println("Pessoas do sexo masculino: " + pessoas_do_sexo_masculino);
+        System.out.println("Pessoas do sexo feminino: " + pessoas_do_sexo_feminino);
+
     }
 }
--- src/Pessoas.java
+++ src/Pessoas.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Pessoas {
+public class Pessoas implements Comparable<Pessoas>{
+    private final String nome;
+    private final String genero;
+
+    public Pessoas(String nome, String genero) {
+        this.nome = nome;
+        this.genero = genero;
+    }
+
+
+    @Override
+    public int compareTo(Pessoas pessoas) {
+        return this.nome.compareTo(pessoas.getNome());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public String getGenero(){
+        return genero;
+    }
 }
